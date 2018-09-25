f6dbf5cac06bbaf4385dbeaadbc8534ef7354688
Cameron Pickle <cmpickle@gmail.com>
2018-09-25 15:44:49 -0600

    Typescritp and loading examples

---
+++ TypeScript/TypeScriptDemo/TypeScriptDemo/Scripts/Car.ts
@@ -0,0 +1,21 @@
+﻿class Car {
+    engine: string;
+
+    constructor(engine: string) {
+        this.engine = engine;
+    }
+
+    start() {
+        alert('Engine started: ' + this.engine);
+    }
+
+    stop() {
+        alert('Engine stopped: ' + this.engine);
+    }
+}
+
+window.onload = function() {
+    let car = new Car('V8');
+    car.start();
+    car.stop();
+}
